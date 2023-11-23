3db85a2f167fcceef9828e3a35d55566c28665f4
Taha Altar Çağ <altarcag@gmail.com>
2023-11-24 00:42:32 +0300

    Update time series analysis.py

--- time series analysis.py
+++ time series analysis.py
@@ -3,13 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("./hydro.csv")
-
 data = data.drop(columns='Unnamed: 0')
 print(data.head())
 
 # Set the 'year' column as the index
 data.set_index('year', inplace=True)
-
 # Calculate the arithmetic mean
 mean_value = data['precipitation (mm)'].mean()
 
@@ -20,7 +18,6 @@ def mean():
     return mean_rounded
 
 mean_res = mean()
-print("arithmetic mean: ", mean_res)
 
 # deviation of precipitation
 pd.set_option('display.float_format', '{:.2f}'.format)
@@ -32,11 +29,21 @@ cum_dev = np.cumsum(pr_dev)
 result_df = pd.DataFrame({'Precipitation': data['precipitation (mm)'], 'Deviation from mean': pr_dev.values, 'Cumulative Deviation': cum_dev})
 # Print the DataFrame
 print(result_df)
+print("arithmetic mean: ", mean_res)
 
-
+plt.figure(figsize=(12, 6))
 data['precipitation (mm)'].plot.bar(width=0.9)
-
 plt.axhline(mean_res, color='red', linestyle='--', label='Mean')
-plt.xlabel("Years", fontsize=15)
-plt.ylabel("P (mm)", fontsize=15)
+plt.xlabel("Years", fontsize=15, labelpad=5)
+plt.ylabel("P (mm)", fontsize=15, labelpad=5)
+plt.title("Precipitation Graph", fontsize=15, pad=10)
+plt.show()
+
+plt.figure(figsize=(12, 6))
+cum_dev.plot()
+plt.xticks(cum_dev.index, rotation=90)
+plt.axhline(0, color='grey', linestyle='--')
+plt.xlabel("Years", fontsize=15, labelpad=5)
+plt.ylabel("P (mm)", fontsize=15, labelpad=5)
+plt.title("Cumulative Deviation of Precipitation graph", fontsize=15, pad=10)
 plt.show()
