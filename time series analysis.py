05fbffee24e249153eec587df8c52d1b2663ce1e
Taha Altar Çağ <altarcag@gmail.com>
2023-11-24 00:45:43 +0300

    Update time series analysis.py

--- time series analysis.py
+++ time series analysis.py
@@ -8,10 +8,9 @@ print(data.head())
 
 # Set the 'year' column as the index
 data.set_index('year', inplace=True)
-# Calculate the arithmetic mean
+#arithmetic mean
 mean_value = data['precipitation (mm)'].mean()
 
-# Print the result
 def mean():
     pr_mean = data['precipitation (mm)'].mean()
     mean_rounded = round(pr_mean, 2)
@@ -25,9 +24,9 @@ pr_dev = data['precipitation (mm)'] - mean_value
 #cumulative deviation
 cum_dev = np.cumsum(pr_dev)
 
-# Combine 'year', 'pr_dev', and 'cum_dev' into a DataFrame
+#combining year, pr_dev, and cum_dev into a DataFrame
 result_df = pd.DataFrame({'Precipitation': data['precipitation (mm)'], 'Deviation from mean': pr_dev.values, 'Cumulative Deviation': cum_dev})
-# Print the DataFrame
+#the DataFrame
 print(result_df)
 print("arithmetic mean: ", mean_res)
 
