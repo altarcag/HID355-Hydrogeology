1a3779399cca398988b66cccb60c2e7e539a76e0
Taha Altar Çağ <altarcag@gmail.com>
2023-11-23 23:33:40 +0300

    Update time series analysis.py

--- time series analysis.py
+++ time series analysis.py
@@ -19,18 +19,21 @@ def mean():
     mean_rounded = round(pr_mean, 2)
     return mean_rounded
 
-
 mean_res = mean()
 print("arithmetic mean: ", mean_res)
 
 # deviation of precipitation
+pd.set_option('display.float_format', '{:.2f}'.format)
 pr_dev = data['precipitation (mm)'] - mean_value
 print(pr_dev)
+#cumulative deviation
+cum_dev = np.cumsum(pr_dev)
+
+print(cum_dev)
 
 data['precipitation (mm)'].plot.bar(width=0.9)
 
 plt.axhline(mean_res, color='red', linestyle='--', label='Mean')
-
 plt.xlabel("Years", fontsize=15)
 plt.ylabel("P (mm)", fontsize=15)
 plt.show()
